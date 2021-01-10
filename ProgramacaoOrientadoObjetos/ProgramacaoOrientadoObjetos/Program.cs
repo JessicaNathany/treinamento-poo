5ecbff27a3961222f50617fba4012e7b5167ca86
Jéssica Nathany Carvalho Freitas <jnathany@outlook.com>
2021-01-10 17:58:01 -0300

    criação da estrutura de pastas para estruturar as aulas

---
+++ ProgramacaoOrientadoObjetos/ProgramacaoOrientadoObjetos/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProgramacaoOrientadoObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ..:: Selecione uma opção ::..");
+            Console.WriteLine();
+        }
+    }
+}
